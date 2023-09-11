1ae6702897e457014a5cc46b0ef317c32cee7e76
Zapishnyi <zapishny@ukr.net>
2023-09-11 17:04:05 +0300

    corected some ul order mistake, added article expand/shorten function on "read more" click

--- js/HTTP_request.js
+++ js/HTTP_request.js
@@ -1,19 +1,37 @@
+// ul created
+let ul_item = document.createElement("ul");
+document.body.appendChild(ul_item);
+// collected data from server
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((json) =>
+    // forming articles list filled with server data
     json.forEach((element) => {
-      let ul_item = document.createElement("ul");
       let li_item = document.createElement("li");
       let span_item = document.createElement("span");
-      span_item.textContent = element.id;
       let h4_item = document.createElement("h4");
-      h4_item.textContent = element.title;
       let p_item = document.createElement("p");
-      p_item.textContent = element.body;
       let a_item = document.createElement("a");
+      span_item.textContent = element.id;
+      h4_item.textContent = element.title;
+      p_item.textContent = element.body;
+      p_item.className = "p" + element.id;
       a_item.textContent = "Read more";
-      document.body.appendChild(ul_item);
+      a_item.className = "a" + element.id;
+      // publish list of aticles to the on the page
       ul_item.appendChild(li_item);
       li_item.append(span_item, h4_item, p_item, a_item);
+      // click 'Read more' event
+      document
+        .querySelector(".a" + element.id)
+        .addEventListener("click", (cliked_item) => {
+          // close all articles readed before
+          document.querySelectorAll("p").forEach((p_css_reset) => {
+            p_css_reset.style.cssText = "display:-webkit-box;";
+          });
+          // open interested article
+          cliked_item.srcElement.offsetParent.childNodes[2].style.cssText =
+            "display:block;";
+        });
     })
   );
